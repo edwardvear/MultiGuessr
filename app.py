feda9787ad4c3ff9bcb392fcaafe0317f902e8fb
Edward Vear <edwardvear@gmail.com>
2020-05-17 20:51:24 -0500

    Added procfile and switched to using environment variables

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, session, redirect, jsonify, abort
 import redis
 
+import os
 import math
 
 app = Flask(__name__)
-r = redis.Redis(host='localhost', port=6379, db=0)
-
-app.secret_key = b'3298afkl;a3ha*"""fsaz'
+r = redis.from_url(os.environ['REDIS_URL'])
+app.secret_key = os.environ['SECRET_KEY']
 
 @app.route('/')
 def index():
